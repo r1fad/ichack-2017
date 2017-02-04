e44ab85448c8adb7bc81c7652a2fe25915d0545f
Rifad Lafir <rifad.lafir@student.manchester.ac.uk>
2017-02-04 19:45:57 +0000

    sentiments for politics and world news complete

---
+++ sentimentAnalysis/sentiment_world.py
@@ -0,0 +1,33 @@
+# Import all modules necessary
+from google.cloud import language
+import json
+import unicodedata
+
+# Instantiates a client
+language_client = language.Client()
+
+#open JSONs
+with open('../news/world/world.json','r') as infile:
+  newsJSON = json.load(infile)
+
+# The text to analyze
+for news_article in newsJSON:
+
+  text = news_article["headline"]
+  text = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+  document = language_client.document_from_text(text)
+
+  # Detects the sentiment of the text
+  sentiment = document.analyze_sentiment()
+
+  news_article["sentiment_score"] = sentiment.score
+  news_article["sentiment_magnitude"] = sentiment.magnitude
+
+  #print('Text: {}'.format(text))
+  #print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+  print news_article
+
+with open('../news/world/world.json','w') as outfile:
+  json.dump(newsJSON,outfile,sort_keys=True,indent=2)
+
